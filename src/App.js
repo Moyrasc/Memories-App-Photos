20a75e232f077e3702f3c2339b8bad433c2fdb6d
Moyrasc <noemisolac@gmail.com>
2023-01-27 14:01:53 +0100

    layout start

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Header from "./components/Header/Header";
+
+
 import "./styles/App.scss";
 
 function App() {
   return (
     <div className="App">
-      <h1>Empezando Proyecto</h1>
+      <Header/>
     </div>
   );
 }

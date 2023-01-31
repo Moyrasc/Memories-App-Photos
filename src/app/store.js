ca7ab9c0d0d29a3684d28ad7e799ce2b7f0780cd
Moyrasc <noemisolac@gmail.com>
2023-01-31 11:57:49 +0100

    created new component : ImageLists

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import imgSliceReducer from '../features/imgSlice/imgSlice';
-
+import favSliceReduce from '../features/favSlice/favSlice';
 
 export const store = configureStore({
   reducer: {
     imgSlice: imgSliceReducer,
+    favSlice: favSliceReduce,
   }
 });

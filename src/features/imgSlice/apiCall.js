cd897713ff2316b2b5132f605c37832dfbf50513
Moyrasc <noemisolac@gmail.com>
2023-02-03 16:05:36 +0100

    small change in apicall

--- src/features/imgSlice/apiCall.js
+++ src/features/imgSlice/apiCall.js
@@ -3,11 +3,11 @@ const {REACT_APP_API_KEY, REACT_APP_URL, REACT_APP_RANDOM} = process.env;
 
 const getPhotos = async (query) => {
     if(!query || query === ""){
-        const response = await fetch(`${REACT_APP_RANDOM}?client_id=${REACT_APP_API_KEY}&count=20`) 
+        const response = await fetch(`${REACT_APP_RANDOM}?client_id=${REACT_APP_API_KEY}&count=10`) 
         const data = await response.json()
         return data
     }else {
-        const response = await fetch(`${REACT_APP_URL}&client_id=${REACT_APP_API_KEY}&query=${query}&per_page=15`)
+        const response = await fetch(`${REACT_APP_URL}&client_id=${REACT_APP_API_KEY}&query=${query}&per_page=10`)
         const data = await response.json()
         return data.results
     }
